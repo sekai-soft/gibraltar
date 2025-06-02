1302973edadfdbaf4eb66830fd1ed1c88fbd5177
KTachibanaM <whj19931115@gmail.com>
2025-06-01 21:16:02 -0700

    add es

--- deploy-apps.sh
+++ deploy-apps.sh
@@ -11,6 +11,7 @@ mkdir -p /home/nixos/mastodon
 cp /home/nixos/gibraltar/apps/mastodon/compose.yml /home/nixos/mastodon/compose.yml
 cp /home/nixos/gibraltar/apps/mastodon/env-mastodon-shared /home/nixos/mastodon/env-mastodon-shared
 cp -r /home/nixos/gibraltar/apps/mastodon/cleanup /home/nixos/mastodon/
+cp -r /home/nixos/gibraltar/apps/mastodon/elasticsearch /home/nixos/mastodon/
 # do not start mastodon
 
 # watchtower
