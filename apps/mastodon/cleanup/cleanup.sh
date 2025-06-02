b7a27c6d6482be66a63618962a37fadf6affe85d
KTachibanaM <whj19931115@gmail.com>
2025-06-01 18:23:59 -0700

    upgrade to 4.3.8

--- apps/mastodon/cleanup/cleanup.sh
+++ apps/mastodon/cleanup/cleanup.sh
@@ -2,11 +2,11 @@
 set -eu
 set -o pipefail
 
+docker exec -it mastodon tootctl accounts cull
+docker exec -it mastodon tootctl accounts prune
 docker exec -it mastodon tootctl media remove --days 14
 docker exec -it mastodon tootctl media remove-orphans
 docker exec -it mastodon tootctl preview_cards remove --days 14
-docker exec -it mastodon tootctl statuses remove --days 14
-docker exec -it mastodon tootctl accounts cull
 
 if [ -f /post.sh ]; then
   echo "Running post script."
