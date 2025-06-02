8ff14f72439d4822714265abe1c27a6162578f44
KTachibanaM <whj19931115@gmail.com>
2025-06-01 17:18:39 -0700

    add post cleanup

--- apps/mastodon/cleanup/cleanup.sh
+++ apps/mastodon/cleanup/cleanup.sh
@@ -7,3 +7,12 @@ docker exec -it mastodon tootctl media remove-orphans
 docker exec -it mastodon tootctl preview_cards remove --days 14
 docker exec -it mastodon tootctl statuses remove --days 14
 docker exec -it mastodon tootctl accounts cull
+
+if [ -f /post.sh ]; then
+  echo "Running post script."
+  chmod +x /post.sh
+  sh /post.sh
+  echo "Ran post script."
+else
+  echo "No post script found."
+fi
